e83f13c16e61436f79c00586af6adb58c07c4be1
Afan <afan.khan50@gmail.com>
2016-03-09 02:45:09 -0500

    Adding Add, Render reviews for a product, changing few images

--- public/project/details/details.controller.js
+++ public/project/details/details.controller.js
@@ -3,44 +3,43 @@
         .module("ShopaholicApp")
         .controller("DetailsController", DetailsController);
 
-    function DetailsController($routeParams, $http, $scope) {
+    function DetailsController($routeParams, $http, $scope, ProductService, $location) {
         var skuId = $routeParams.skuId;
-        //var apiKey = ay4rd26c7bqjh9zutd5ynkm6
+
         $http.get("http://api.bestbuy.com/v1/products/" + skuId + ".json?apiKey=ay4rd26c7bqjh9zutd5ynkm6")
             .success(renderProduct);
 
-        $http.get("http://api.bestbuy.com/v1/reviews(sku="+skuId+")?format=json&apiKey=ay4rd26c7bqjh9zutd5ynkm6")
-            .success(renderReview);
+        //$http.get("http://api.bestbuy.com/v1/reviews(sku="+skuId+")?format=json&apiKey=ay4rd26c7bqjh9zutd5ynkm6")
+        //    .success(renderReview);
 
 
 
         $scope.callUpdate = callUpdate;
+        renderReview(skuId);
+        $scope.addReview = addReview;
 
         function callUpdate() {
 
             $('.star-rating').raty({
                 path: 'images/',
-                readOnly: true,
                 score: function () {
                     return $(this).attr('data-score');
                 }
             });
         }
 
-
         function renderProduct(response) {
             $scope.product = response;
         }
 
-        function renderReview(response) {
-            //console.log(response);
-            $scope.dataReview = response;
-            console.log($scope.dataReview);
-
-            console.log($);
+        function renderReview(skuId) {
+            $scope.reviewsData = ProductService.findAllReviewsForProduct(skuId);
+            console.log($scope.reviewsData);
         }
 
-
+        function addReview(reviewObject){
+            $scope.reviewsData = ProductService.createReview(234,$routeParams.skuId,reviewObject);
+        }
 
     }
 })();
--- public/project/services/product.service.js
+++ public/project/services/product.service.js
@@ -4,10 +4,26 @@
         .factory("ProductService", ProductService);
 
     function ProductService($http) {
+
+        var reviews = [
+            {"userId" : 234,
+                "title": "This is a test title 1",
+                "review" : "This is a test review for macbook pro. Product ID: 1219696697139",
+                "rating": 4,
+                "productId" : "7639085"},
+
+            {"userId" : 345,
+                "title": "This is a test title 2",
+                "review" : "This is a test review for macbook pro. Product ID: 1219696697139",
+                "rating": 4,
+                "productId" : "7619002"}
+        ];
+
         var api = {
             findProductsByTitle: findProductsByTitle,
-            //findProductBySkuId: findProductBySkuId,
-            browseProductsByCategory: browseProductsByCategory
+            browseProductsByCategory: browseProductsByCategory,
+            findAllReviewsForProduct: findAllReviewsForProduct,
+            createReview:createReview
         };
         return api;
 
@@ -17,15 +33,40 @@
                 .success(callback);
         }
 
-        //function findProductBySkuId(skuId){
-        //
-        //}
-
         function browseProductsByCategory(category, callback){
             var categoryParam = category;
-            //alert("IN product service "+categoryParam);
             $http.get("https://api.bestbuy.com/v1/products((categoryPath.id="+categoryParam+"))?apiKey=ay4rd26c7bqjh9zutd5ynkm6&facet=bestSellingRank&pageSize=20&format=json")
                 .success(callback);
         }
+
+
+        function findAllReviewsForProduct(productId){
+            var reviewParam = productId;
+            var toReturn = reviews.filter(function(review){
+                return (review.productId === reviewParam);
+            });
+            return toReturn;
+        }
+
+        function createReview(userId, productId, review) {
+            var currReview = {
+                "userId": userId,
+                "productId": productId,
+                "title": review.title,
+                "review": review.review,
+                "rating": 2
+
+            };
+            reviews.push(currReview);
+            review.title = null;
+            review.rating = null;
+            review.review = null;
+            review.user = null;
+            review.productId = null;
+            console.log(reviews);
+
+            return findAllReviewsForProduct(productId);
+        }
     }
 })();
+
